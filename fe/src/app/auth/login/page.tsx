b8210691ff7c3180cdb4876b67484c545036f05c
dobos <doboszdav@gmail.com>
2023-11-01 00:34:42 +0100

    login page added

---
+++ fe/src/app/auth/login/page.tsx
@@ -0,0 +1,108 @@
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {signIn} from "@/services/auth-service";
+import {AuthResponse, LoginDto} from "@/types/auth/auth-types";
+import {useDispatch} from "react-redux";
+import {setAuthentication} from "@/redux/features/auth/auth-slice";
+import {setToken} from "@/services/token-service";
+import {useRouter} from "next/navigation";
+
+export default function Login() {
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const loginData: LoginDto = {
+      email: formData.get('email')?.toString(),
+      password: formData.get('password')?.toString(),
+    };
+    signIn(loginData)
+    .then((response: AuthResponse) => {
+      dispatch(setAuthentication(true));
+      setToken(response.token);
+      router.push('/notes/my-notes');
+    })
+  };
+
+  return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+        >
+          <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+            <LockOutlinedIcon/>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{mt: 1}}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+            />
+            <FormControlLabel
+                control={<Checkbox value="remember" color="primary"/>}
+                label="Remember me"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{mt: 3, mb: 2}}
+                color={'primary'}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+  );
+}
